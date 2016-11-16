e7a1d2948cba409871e9ec7140e7c51b46e1b951
Steve Hu <stevehu@gmail.com>
2016-11-16 18:27:34 -0500

    add jooby

---
+++ jooby/src/main/java/com/mycompany/App.java
@@ -0,0 +1,18 @@
+package com.mycompany;
+
+import org.jooby.Jooby;
+
+/**
+ * @author jooby generator
+ */
+public class App extends Jooby {
+
+  {
+    get("/", () -> "Hello World!");
+  }
+
+  public static void main(final String[] args) {
+    run(App::new, args);
+  }
+
+}
