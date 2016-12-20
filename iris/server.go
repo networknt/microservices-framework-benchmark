aad60bd32c03070a04cc2b4ae95497308578ef2d
Steve Hu <stevehu@gmail.com>
2016-12-20 15:40:35 -0500

    add iris

---
+++ iris/server.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/kataras/iris"
+
+func main() {
+	iris.Config.Charset = "UTF-8" // this is the default, you don't have to set it manually
+
+	iris.Get("/", func(ctx *iris.Context) {
+		ctx.Text(iris.StatusOK, "Hello World!")
+	})
+	iris.Listen(":8080")
+}
