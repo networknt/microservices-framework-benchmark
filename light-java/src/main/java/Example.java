604c6529f822a12f74f987a250b0263c0efce675
Steve Hu <stevehu@gmail.com>
2017-02-23 22:07:04 -0500

    add content type

--- light-java/src/main/java/Example.java
+++ light-java/src/main/java/Example.java
@@ -2,6 +2,7 @@ import com.networknt.server.HandlerProvider;
 import io.undertow.Handlers;
 import io.undertow.server.HttpHandler;
 import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
 
 
 public class Example implements HandlerProvider {
@@ -11,6 +12,7 @@ public class Example implements HandlerProvider {
         return Handlers.path()
         .addPrefixPath("/", new HttpHandler() {
                 public void handleRequest(HttpServerExchange exchange) {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
                     exchange.getResponseSender().send(MESSAGE);
             	}
             }
