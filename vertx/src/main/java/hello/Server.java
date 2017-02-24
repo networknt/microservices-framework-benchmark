952190145959438d4f12a47551ade086c4e4a021
Sean Burns <seanjburns@gmail.com>
2017-02-24 09:51:45 +0200

    Added response headers (Server,Date) to be more inline with other tests

--- vertx/src/main/java/hello/Server.java
+++ vertx/src/main/java/hello/Server.java
@@ -3,10 +3,13 @@ package hello;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpHeaders;
+
+import java.util.Date;
 
 public class Server extends AbstractVerticle {
 
-    private static final int INSTANCES = 6;
+    private static final int INSTANCES = 4;
     private static final String TEXT = "Hello World!";
 
     // Convenience method so you can run it in your IDE
@@ -18,7 +21,10 @@ public class Server extends AbstractVerticle {
     @Override
     public void start() throws Exception {
         vertx.createHttpServer().requestHandler(req -> {
-            req.response().end(TEXT);
+            req.response()
+                    .putHeader(HttpHeaders.SERVER, "Vertx")
+                    .putHeader(HttpHeaders.DATE, new Date().toString())
+                    .end(TEXT);
         }).listen(8080);
     }
 }
