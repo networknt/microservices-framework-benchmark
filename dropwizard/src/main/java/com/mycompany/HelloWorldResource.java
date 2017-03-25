c52fc9d1766656c420e6991d1df39d4c8c4fde26
David Duong <david.nqd@gmail.com>
2017-03-24 20:55:00 -0700

    Dropwizard HelloWorldResource is now marked as javax.inject.Singleton

--- dropwizard/src/main/java/com/mycompany/HelloWorldResource.java
+++ dropwizard/src/main/java/com/mycompany/HelloWorldResource.java
@@ -1,13 +1,14 @@
 package com.mycompany;
 
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.inject.Singleton;
 
 @Path("/")
 @Produces(MediaType.TEXT_PLAIN)
+@Singleton
 public class HelloWorldResource {
     @GET
     public String sayHello() {
