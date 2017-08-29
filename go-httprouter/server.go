585e029fc40a6df7a71f758cfa1c2675321bc98b
Rodrigo Mata <rodrigo.mata.ib@gmail.com>
2017-08-29 11:51:42 -0500

    chore: Add Golang HttpRouter benchmark

---
+++ go-httprouter/server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+    "github.com/julienschmidt/httprouter"
+    "net/http"
+)
+
+func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+    fmt.Println("Hello World!")
+}
+
+func main() {
+    router := httprouter.New()
+    router.GET("/", Hello)
+
+    log.Fatal(http.ListenAndServe(":8080", router))
+}
