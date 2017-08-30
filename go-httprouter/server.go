fb0afb3471a41e69236b046b4f9d1e7de0ad4cf0
Rodrigo Mata <rodrigo.mata.ib@gmail.com>
2017-08-30 09:08:14 -0500

    fix: changed hello world message to display it in console

--- go-httprouter/server.go
+++ go-httprouter/server.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-    "fmt"
+    "io"
     "github.com/julienschmidt/httprouter"
     "net/http"
 )
 
 func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-    fmt.Println("Hello World!")
+  io.WriteString(w, "Hello World!")
 }
 
 func main() {
-    router := httprouter.New()
-    router.GET("/", Hello)
+  router := httprouter.New()
+  router.GET("/", Hello)
 
   http.ListenAndServe(":8080", router)
 }
