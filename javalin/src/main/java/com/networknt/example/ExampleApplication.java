6ae0fcca86bba115c1b427c2d0f54645f5d46785
Jindrich Pilar <pilar.jindrich@gmail.com>
2020-03-16 16:28:39 +0100

    Javalin enable TLS and HTTP/2

--- javalin/src/main/java/com/networknt/example/ExampleApplication.java
+++ javalin/src/main/java/com/networknt/example/ExampleApplication.java
@@ -1,12 +1,65 @@
 package com.networknt.example;
 
 import io.javalin.Javalin;
+import org.eclipse.jetty.alpn.server.ALPNServerConnectionFactory;
+import org.eclipse.jetty.http2.HTTP2Cipher;
+import org.eclipse.jetty.http2.server.HTTP2ServerConnectionFactory;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.server.SslConnectionFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class ExampleApplication {
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create().start(8080);
+        Javalin app = Javalin.create(config -> {
+            config.server(ExampleApplication::createServer);
+        }).start();
         app.get("/", ctx -> ctx.result("Hello world!"));
     }
 
+    private static Server createServer() {
+        Server server = new Server();
+
+        // HTTP
+        ServerConnector connector = new ServerConnector(server);
+        connector.setPort(8080);
+        server.addConnector(connector);
+
+        // TLS + HTTP2
+        HttpConfiguration httpConfig = new HttpConfiguration();
+        httpConfig.setSendServerVersion(false);
+        httpConfig.setSecureScheme("https");
+        httpConfig.setSecurePort(8443);
+
+        SslContextFactory sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath(ExampleApplication.class.getResource("/server.keystore").toExternalForm());
+        sslContextFactory.setKeyStorePassword("password");
+        sslContextFactory.setCipherComparator(HTTP2Cipher.COMPARATOR);
+        sslContextFactory.setProvider("Conscrypt");
+
+        HttpConfiguration httpsConfig = new HttpConfiguration(httpConfig);
+        httpsConfig.addCustomizer(new SecureRequestCustomizer());
+
+        HTTP2ServerConnectionFactory h2 = new HTTP2ServerConnectionFactory(httpsConfig);
+        ALPNServerConnectionFactory alpn = new ALPNServerConnectionFactory();
+        alpn.setDefaultProtocol("h2");
+
+        SslConnectionFactory ssl = new SslConnectionFactory(sslContextFactory, alpn.getProtocol());
+
+        ServerConnector http2Connector = new ServerConnector(
+                server,
+                ssl,
+                alpn,
+                h2,
+                new HttpConnectionFactory(httpsConfig)
+        );
+        http2Connector.setPort(8443);
+        server.addConnector(http2Connector);
+
+        return server;
+    }
 }
