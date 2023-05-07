45bb5e37dd54ada0fc4446405082221d0edf4684
Tran Ngoc Khoa <tranngoc.khoa.spk@gmail.com>
2023-05-07 21:55:40 +0700

    Add Spring Boot 3 undertow

---
+++ spring-boot-3-undertow/src/main/java/demo/SpringBoot3TomcatApplication.java
@@ -0,0 +1,20 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBoot3TomcatApplication {
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3TomcatApplication.class, args);
+    }
+
+}
---
+++ spring-boot-3-undertow/src/test/java/demo/SpringBoot3TomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
