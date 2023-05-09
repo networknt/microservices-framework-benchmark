15366c527be19873a088df23e58a1fcf771c442e
Trần Ngọc Khoa <30297271+TranNgocKhoa@users.noreply.github.com>
2023-05-10 04:43:26 +0700

    Add spring Boot 3 Tomcat (#109)
    Co-authored-by: Khoa/Kaden/개발지원팀 <khoa.tran@celebe.io>

---
+++ spring-boot-3-tomcat/src/test/java/com/example/springboot3tomcat/SpringBoot3TomcatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot3tomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3TomcatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
