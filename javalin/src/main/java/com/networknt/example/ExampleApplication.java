9ef6bb36d30748c2466affc209f96126f144f9b0
vudangngoc <vudangngoc@gmail.com>
2024-11-01 14:29:58 +0700

    add HTTS

--- javalin/src/main/java/com/networknt/example/ExampleApplication.java
+++ javalin/src/main/java/com/networknt/example/ExampleApplication.java
@@ -1,13 +1,35 @@
 package com.networknt.example;
 
 import io.javalin.Javalin;
+import org.eclipse.jetty.server.ServerConnector;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 public class ExampleApplication {
 
     public static void main(String[] args) {
-        Javalin app = Javalin.create(/*config*/)
-                .get("/", ctx -> ctx.result("Hello World"))
-                .start(8080);
+        // Get example from https://github.com/javalin/javalin/blob/master/javalin/src/test/java/io/javalin/examples/HelloWorldSecure.java#L22-L30
+        Javalin.create(config -> {
+                    config.jetty.addConnector((server, httpConfiguration) -> {
+                        ServerConnector sslConnector = new ServerConnector(server, getSslContextFactory());
+                        sslConnector.setPort(443);
+                        return sslConnector;
+                    });
+                    config.jetty.addConnector((server, httpConfiguration) -> {
+                        ServerConnector connector = new ServerConnector(server);
+                        connector.setPort(80);
+                        return connector;
+                    });
 
+                })
+                .start()
+                .get("/", ctx -> ctx.result("Hello World"));
+
+    }
+
+    private static SslContextFactory.Server getSslContextFactory() {
+        SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
+        sslContextFactory.setKeyStorePath(ExampleApplication.class.getResource("/keystore.jks").toExternalForm());
+        sslContextFactory.setKeyStorePassword("password");
+        return sslContextFactory;
     }
 }
